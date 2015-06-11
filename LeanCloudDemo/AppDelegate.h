3c86855db12dfe268c160511ada6118bfa86968d
EaseMobIMDemo <425035106@qq.com>
2015-06-11 14:06:26 +0800

    Initial Commit

---
+++ LeanCloudDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeanCloudDemo
+//
+//  Created by Mac on 15-6-11.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeanCloudDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeanCloudDemo
+//
+//  Created by Mac on 15-6-11.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
